422d92e1d98c907ced59b7fd62bba669fcde22d4
Aden Crace <104579388+Acrace13@users.noreply.github.com>
2022-05-04 11:18:53 -0400

    added coins and score

--- sketch.js
+++ sketch.js
@@ -2,12 +2,16 @@ kaboom()
 
 loadBean()
 
+let score =0
+
 const player = add ([
   sprite("bean"),
   pos(50,300),
   body(),
   area(),
 ])
+
+
 
 add ([
   rect(200,50),
@@ -35,6 +39,14 @@ add ([
   area(),
 ])
 
+add([
+  "coin", // coin 1
+  rect(100,200),
+  pos(400,250),
+  area(),
+  color(200,200,0)
+])
+
 add ([ //tube 2
   rect(100,175),
   pos(900,0),
@@ -53,6 +65,14 @@ add ([
   area(),
 ])
 
+add([
+  "coin", // coin 2
+  rect(100,100),
+  pos(900,250),
+  area(),
+  color(200,200,0)
+])
+
 add ([ //tube 3
   rect(100,200),
   pos(1400,0),
@@ -69,7 +89,17 @@ add ([
   color(0,100,0),
   solid(),
   area(),
+  ])
+  
+  
+add([
+  "coin", // coin 3
+  rect(100,200),
+  pos(1400,250),
+  area(),
+  color(200,200,0)
 ])
+  
 add ([ //tube 4
   rect(100,300),
   pos(1900,0),
@@ -87,6 +117,46 @@ add ([
   solid(),
   area(),
 ])
+
+
+add([
+  "coin", // coin 4
+  rect(100,150),
+  pos(1900,370),
+  area(),
+  color(200,200,0)
+])
+
+add ([ //tube 5
+  rect(100,100),
+  pos(2400,0),
+  color(0,100,0),
+  solid(),
+  area(),
+  "tube"
+])
+
+add ([  
+  "tube", 
+  rect(100,600),
+  pos(2400,350),
+  color(0,100,0),
+  solid(),
+  area(),
+])
+
+let scoreLable = add([
+  text ("Score: 0"),
+  pos(10,10)
+])
+
+
+player.onCollide("coin", (c) => {
+  destroy(c)
+  score++
+scoreLable.text = "score: " +score
+})
+
 
 player.onCollide("tube", () => {
   addKaboom(player.pos)
